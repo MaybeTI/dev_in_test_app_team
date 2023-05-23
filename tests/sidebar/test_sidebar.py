531138f452f3807a901f64da442c022b517a3c28
Bohdan Lysachenko <lysachenko111.ukr.net>
2023-05-23 15:43:57 +0300

    Some changes

--- tests/sidebar/test_sidebar.py
+++ tests/sidebar/test_sidebar.py
@@ -1,6 +1,7 @@
 import pytest
 import logging
 
+from appium.webdriver.common.mobileby import MobileBy
 
 logging.basicConfig(filename='test.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -11,6 +12,11 @@ logging.basicConfig(filename='test.log', level=logging.INFO, format='%(asctime)s
         ("qa.ajax.app.automation@gmail.com", "qa_automation_password", True)
      ]
                          )
-def test_user_login(user_login_fixture, email: str, password: str, result: bool) -> None:
+def test_sidebar(user_login_fixture, email: str, password: str, result: bool) -> None:
     logging.info(f"Running test_user_login with email: {email}, password: {password}")
     assert user_login_fixture.sidebar(email, password) == result
+    assert user_login_fixture.find_element(MobileBy.ID, "com.ajaxsystems:id/addHub").text == "Add hub"
+    assert user_login_fixture.find_element(MobileBy.ID, "com.ajaxsystems:id/settings").text == "App Settings"
+    assert user_login_fixture.find_element(MobileBy.ID, "com.ajaxsystems:id/help").text == "Help"
+    assert user_login_fixture.find_element(MobileBy.ID, "com.ajaxsystems:id/logs").text == "Report a problem"
+
